3fd0b3250c21f252b2c5cd1e86b79f5f7ec34ffe
Carlos Vivas <65358778+cabjkiko@users.noreply.github.com>
2023-04-04 20:16:26 -0300

    Created function 'playRound'

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function getComputerChoice(){
-let choices = ['Rock', 'Paper', 'Scissors'];
-let thing = choices[Math.floor(Math.random()*choices.length)];
-console.log("The computer's choice is: "+ thing);
+let choices = ['rock', 'paper', 'scissors'];
+let randomIndex = Math.floor(Math.random()*3);
+
+return choices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection){
+
+    playerSelection = playerSelection.toLowerCase();
+
+    //Check for a tie
+    if ( playerSelection === computerSelection){
+        return "It's a tie!";
+    }
+
+    //Check for player win scenarios
+    if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+
+    //If it's not a tie and player doesn't win, then it's a loss
+    return `You Lose! ${computerSelection} beats ${playerSelection}`
 }
